95ff42b40811a8026acd90d73c39c7d4fe71e325
isaacbolili2001-debug <isaacbolili2001@gmail.com>
2026-08-31 13:40:07 +0100

    add some code

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        
+        Console.WriteLine("what is favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your favorite color is: {color}");
     }
 }
