cf313816b95a29462c6c986e65ced9bcf953431e
isaac bolili <ibolilibakambala@byupathway.edu>
2026-09-01 18:58:18 +0100

    cool

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Random randomGenerator = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+            int number = randomGenerator.Next(1, 101); // Generates a random number between 1 and 100
+            numbers.Add(number);
+        }
+        Console.WriteLine("The generated numbers are:");
+        foreach (int n in numbers)
+        {
+            Console.WriteLine(n);
+        }
     }
 }
