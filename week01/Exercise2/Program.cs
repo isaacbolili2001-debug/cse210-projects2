8c9ba51382cfc8796a1581d7960a98dd27632461
isaac bolili <ibolilibakambala@byupathway.edu>
2026-09-01 13:38:35 +0100

    i just did this

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -6,9 +6,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
-        
+
         Console.Write("what is your grade? :");
-        string userInput=Console.ReadLine();
+        string userInput = Console.ReadLine();
         int grade = int.Parse(userInput);
 
         string letter = "";
@@ -16,27 +16,27 @@ class Program
         if (grade >= 90)
         {
             letter = "A";
-            
+
         }
         else if (grade >= 80)
         {
             letter = "B";
-           
+
         }
         else if (grade >= 70)
         {
             letter = "C";
-           
+
         }
         else if (grade >= 60)
         {
             letter = "D";
-           
+
         }
         else
         {
             letter = "F";
-            
+
         }
         Console.WriteLine($"Your letter grade is: {letter}");
         if (letter == "A" || letter == "B" || letter == "C")
@@ -47,6 +47,7 @@ class Program
         {
             Console.WriteLine("Sorry, you did not pass the class.");
             Console.WriteLine("Better luck next time!");
+
         }
 
     }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11); // Generates a random number between 1 and 10
+
+        Console.WriteLine("Please guess a number between 1 and 10: ");
+        string input = Console.ReadLine();
+        int guess = int.Parse(input);
+
+
+
+
+        while (guess != number)
+        {
+            if (guess > number)
+            {
+                Console.WriteLine("go lower.");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("go higher.");
+            }
+
+            Console.WriteLine("Please guess again: ");
+            input = Console.ReadLine();
+            guess = int.Parse(input);
+        }
+
+        Console.WriteLine($"Congratulations! You guessed the correct number: {number}");
+
     }
 }
