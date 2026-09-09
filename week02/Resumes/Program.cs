8323edf1e804fd6a6d6036b0d2cb467ae8228270
isaac bolili <ibolilibakambala@byupathway.edu>
2026-09-09 16:00:40 +0100

    worked on the resume project.

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Web Developer";
+        job2._company = "Google";
+        job2._startYear = 2018;
+        job2._endYear = 2021;
+
+        Resume myResume = new Resume();
+        myResume._name = "Isaac bolili";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
