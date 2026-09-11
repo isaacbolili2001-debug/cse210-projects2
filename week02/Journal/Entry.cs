87e8f0e35f92260d287490cf69b83c7fda80ad3c
isaac bolili <ibolilibakambala@byupathway.edu>
2026-09-11 13:07:17 +0100

    fixed date time.

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -19,7 +19,7 @@ public class Entry
 
     public Entry(string promptText, string entryText)
     {
-        _date = DateTime.Now.ToString("yyyy-MM-dd");
+        _date = DateTime.Now.ToString("yyyy/MM/dd");
         _promptText = promptText;
         _entryText = entryText;
     }
